a76d95597ef415fa6a5655b8f900ed6036039272
Quốc Anh Lê <anhlq.uit@gmail.com>
2020-03-30 19:36:50 +0700

    TestNG

---
+++ selenium/testing_framework/Part_II_Group.java
@@ -0,0 +1,56 @@
+package testing_framework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Part_II_Group {
+	WebDriver driver;
+	
+  @BeforeClass
+  public void beforeClass() {
+	  System.out.println("beforeClass");
+
+	  driver = new FirefoxDriver();
+	  Assert.assertTrue(false);
+  }
+	
+  @Test(groups = "user")
+  public void TC_01() {
+	  System.out.println("Run TC_01");
+  }
+  
+  @Test(groups = "user")
+  public void TC_02() {
+	  System.out.println("Run TC_02");
+  }
+  
+  @Test(groups = "pay")
+  public void TC_03() {
+	  System.out.println("Run TC_03");
+  }
+  
+  @Test(groups = "pay")
+  public void TC_04() {
+	  System.out.println("Run TC_04");
+  }
+  
+  @Test(groups = "shop")
+  public void TC_05() {
+	  System.out.println("Run TC_05");
+  }
+  
+  @Test(groups = "shop")
+  public void TC_06() {
+	  System.out.println("Run TC_06");
+  }
+  
+  @AfterClass(alwaysRun = true)
+  public void afterClass() {
+	  System.out.println("afterClass");
+	  driver.quit();
+  }	
+}
---
+++ selenium/testing_framework/Part_I_Annotations.java
@@ -0,0 +1,62 @@
+package testing_framework;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class Part_I_Annotations {
+  @Test
+  public void TC_01() {
+	  System.out.println("Run TC_01");
+  }
+  @Test
+  public void TC_02() {
+	  System.out.println("Run TC_02");
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("beforeMethod");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("afterMethod");
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+	  System.out.println("beforeClass");
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  System.out.println("afterClass");
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+	  System.out.println("beforeTest");
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  System.out.println("afterTest");
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+	  System.out.println("beforeSuite");
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+	  System.out.println("afterSuite");
+  }
+
+}
